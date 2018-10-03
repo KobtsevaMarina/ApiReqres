08b5f37068f02144e9a308d11e8ea52adcfc9527
Марина Кобцева <M.A.Kobtseva@hotmail.com>
2018-10-03 12:44:13 +0700

    Добавьте файлы проекта.

---
+++ ApiReqres/ApiReqres/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ApiReqres
+{
+	public partial class MainPage : ContentPage
+	{
+        static int page;
+        public MainPage()
+		{
+			InitializeComponent();
+            ClickAhead(null, null);
+        }
+        private void GetAheadListUsers()
+        {
+            List<User> users = new List<User>();
+            var reqresApi = RestService.For<IReqresApi>("https://reqres.in/api");
+            for (int i = page * 3 + 1; i <= page * 3 + 3; i++)
+            {
+                var apiResult = reqresApi.GetUser(i).Result;
+                User user = new User(apiResult);
+                users.Add(user);
+            }
+            listApi.ItemsSource = users;
+            page++;
+        }
+        private void GetBackListUsers()
+        {
+            page = page - 2;
+            List<User> users = new List<User>();
+            var reqresApi = RestService.For<IReqresApi>("https://reqres.in/api");
+            for (int i = page * 3 + 1; i <= page * 3 + 3; i++)
+            {
+                var apiResult = reqresApi.GetUser(i).Result;
+                User user = new User(apiResult);
+                users.Add(user);
+            }
+            listApi.ItemsSource = users;
+            page++;
+        }
+        public interface IReqresApi
+        {
+            [Get("/users/{user}")]
+            Task<String> GetUser(int user);
+        }
+        private void ClickBack(object sender, EventArgs e)
+        {
+            State.TextColor = Color.Default;
+            State.Text = "Загрузка";
+            try
+            {
+                GetBackListUsers();
+                State.Text = "Выполнено";
+                if (page < 2)
+                    ButtonBack.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                State.Text = "Ошибка при загрузки";
+                State.TextColor = Color.Accent;
+            }
+        }
+        private void ClickAhead(object sender, EventArgs e)
+        {
+            State.TextColor = Color.Default;
+            State.Text = "Загрузка";
+            try
+            {
+                GetAheadListUsers();
+                State.Text = "Выполнено";
+                ButtonBack.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                State.Text = "Ошибка при загрузки";
+                State.TextColor = Color.Accent;
+            }
+        }
+    }
+}
---
+++ ApiReqres/ApiReqres/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ApiReqres
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+
+        public User(string apiResult)
+        {
+            Id = Int32.Parse(Regex.Match(apiResult, @"""id"":(\w*),").Groups[1].Value);
+            FirstName = Regex.Match(apiResult, "\"first_name\":\"(\\w*)\",").Groups[1].Value;
+            LastName = Regex.Match(apiResult, "\"last_name\":\"(\\w*)\",").Groups[1].Value;
+            Avatar = Regex.Match(apiResult, "\"avatar\":\"(\\S*)\"").Groups[1].Value;
+        }
+    }
+}
